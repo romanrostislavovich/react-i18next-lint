e1449b3dcbc05b8ce2a350598cd3816bc063da72
Roman <romanrostislavovich@gmail.com>
2025-10-17 15:27:30 +0300

    feat: update core modules

--- src/core/utils/http.ts
+++
@@ -1,9 +0,0 @@
-export class Http {
-    // tslint:disable-next-line:no-any
-    public static async get(url: string): Promise<any> {
-        return await fetch(url).then(res => res.json()).then(res => {
-            const space: number  = 4;
-            return  JSON.stringify(res, null, space);
-        });
-    }
-}
